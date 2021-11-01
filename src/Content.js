1785275bcc0b4a4f7766886822845427030c693d
Amber Custer <acuster@madisoncollege.edu>
2021-11-01 16:12:53 -0500

    Set up state for an artist variable, onClick calls handleRecommendation which sets the artist

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,22 @@
-const Container = () => {
+import { useState } from 'react'
+
+const Content = () => {
+    const [artist, setArtist] = useState('Give Me Artists Like...')
+
+    const handleRecommendation = () => {
+        const artists = ["Buddy Holly", "The Beatles", "David Bowie"]
+        const int = Math.floor(Math.random() * 3)
+
+        setArtist(artists[int])
+    }
+
     return (
         <main>
             <h2>Recommending new jams based on music you already love</h2>
+            <p>{artist}</p>
+            <button onClick={handleRecommendation}>Click It</button>
         </main>
     )
 }
 
-export default Container
+export default Content
