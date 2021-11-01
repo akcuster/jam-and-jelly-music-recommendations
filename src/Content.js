85dc0fb31901aee9476c0d5f99b865008d3f0cca
Amber Custer <acuster@madisoncollege.edu>
2021-11-01 13:46:58 -0500

    Create content and footer components with basic filler text

---
+++ src/Content.js
@@ -0,0 +1,9 @@
+const Container = () => {
+    return (
+        <main>
+            <h2>Recommending new jams based on music you already love</h2>
+        </main>
+    )
+}
+
+export default Container
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Created By: Amber Custer 2021</p>
+        </footer>
+    )
+}
+
+export default Footer
