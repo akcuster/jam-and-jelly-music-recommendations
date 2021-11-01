9231118cb4f7e3557e8b68f880c3e0e5679ffad1
Amber Custer <acuster@madisoncollege.edu>
2021-11-01 18:00:02 -0500

    Get rid of unused button

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
-const Content = ({ artist, handleSearch }) => {
+const Content = ({ artist }) => {
 
     return (
         <main>
             <h2>Recommending new jams based on music you already love</h2>
             <p>{artist}</p>
-            <button onClick={handleSearch}>Click It</button>
+
         </main>
     )
 }
