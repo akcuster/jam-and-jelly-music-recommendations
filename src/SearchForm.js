366dd6ba74637bce81b86b6d24c77357738d8728
Amber Custer <acuster@madisoncollege.edu>
2021-11-02 17:56:45 -0500

    Rename Search component to SearchForm

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,12 +1,17 @@
+import { useRef } from 'react'
+
 const Search = ({ artist, setArtist, handleSubmit}) => {
+    const inputRef = useRef()
     return (
         <form className='searchForm' onSubmit={handleSubmit}>
             {/*TODO: style label to be hidden*/}
             {/*<label htmlFor='search'>Get Recommendations</label>*/}
             <input
                 autoFocus
+                ref={inputRef}
                 id='search'
                 type='text'
+                role='searchbox'
                 placeholder='Give Me Artists Like...'
                 required
                 value={artist}
@@ -15,6 +20,7 @@ const Search = ({ artist, setArtist, handleSubmit}) => {
             <button
                 type='submit'
                 aria-label='Get Recommendations'
+                onClick={() => inputRef.current.focus()}
             >
                 Get Recommendations
             </button>
